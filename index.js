5dedeeea69ce8c9614a52376174ec48a3960797e
pauline-wim <linette.wim@hotmail.com>
2022-03-08 14:20:51 +0100

    Add get & post request => read students list, create new student in body

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const students = [
+  {
+    id: 1,
+    name: "Paul",
+  },
+  {
+    id: 2,
+    name: "Bob",
+  },
+  {
+    id: 3,
+    name: "Michael",
+  },
+];
+
 // ROUTES
 
 app.get("/", (_req, res) => {
-  res.send("Start");
+  res.send("Students");
+});
+
+app.get("/students", (_req, res) => {
+  res.json(students);
+});
+
+app.post("/students", (req, res) => {
+  //   console.log(req.body);
+  students.push({
+    id: students.length + 1,
+    name: req.body.name,
+  });
+  res.send(students);
 });
 
 // ERROR
